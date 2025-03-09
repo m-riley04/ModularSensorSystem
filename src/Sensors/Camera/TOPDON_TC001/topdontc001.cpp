8e3de7964c398b90441bdc41cee1298560f4282f
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-03-08 19:46:16 -0600

    add template TS001 camera class and libusb

---
+++ src/Sensors/Camera/TOPDON_TC001/topdontc001.cpp
@@ -0,0 +1,8 @@
+#include "topdontc001.h"
+
+TopdonTC001::TopdonTC001(QObject *parent)
+	: QObject(parent)
+{}
+
+TopdonTC001::~TopdonTC001()
+{}
---
+++ src/Sensors/Camera/TOPDON_TC001/topdontc001.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <QObject>
+
+enum TC001Pallette
+{
+	WHITE_HOT,
+	BLACK_HOT,
+	IRONBOW,
+	RED_HOT,
+	RAINBOW,
+	JUNGLE,
+	AURORA,
+	CITY,
+	NIGHT,
+	SEPIA,
+	LAVA
+};
+
+class TopdonTC001  : public QObject
+{
+	Q_OBJECT
+
+private:
+
+	double mGlobalTemperature;
+	
+	/// CONTROLS
+
+	double mAtmosphericTemperature;
+	double mReflectionTemperature;
+	double mDistance;
+	double mHumidity;
+	double mEmmisivity;
+	bool mIsTemperatureBar;
+	bool mIsIsothermal;
+	bool mIsStretchMode;
+	TC001Pallette mPallette;
+
+public:
+	TopdonTC001(QObject *parent);
+	~TopdonTC001();
+
+	double globalTemperature() const { return mGlobalTemperature; };
+
+	/// CONTROLS
+
+	double atmosphericTemperature() const { return mAtmosphericTemperature; };
+	double reflectionTemperature() const { return mReflectionTemperature; };
+	double distance() const { return mDistance; };
+	double humidity() const { return mHumidity; };
+	double emmisivity() const { return mEmmisivity; };
+	bool isTemperatureBar() const { return mIsTemperatureBar; };
+	bool isIsothermal() const { return mIsIsothermal; };
+	bool isStretchMode() const { return mIsStretchMode; };
+	TC001Pallette pallette() const { return mPallette; };
+
+public slots:
+
+	/// CONTROLS
+
+	void setAtmosphericTemperature(double temperature);
+	void setReflectionTemperature(double temperature);
+	void setDistance(double distance);
+	void setHumidity(double humidity);
+	void setEmmisivity(double emmisivity);
+	void setTemperatureBar(bool isTemperatureBar);
+	void setIsothermal(bool isIsothermal);
+	void setStretchMode(bool isStretchMode);
+
+signals:
+	void globalTemperatureChanged(double temperature);
+
+	/// CONTROLS
+	void atmosphericTemperatureChanged(double temperature);
+	void reflectionTemperatureChanged(double temperature);
+	void distanceChanged(double distance);
+	void humidityChanged(double humidity);
+	void emmisivityChanged(double emmisivity);
+	void temperatureBarChanged(bool isTemperatureBar);
+	void isothermalChanged(bool isIsothermal);
+	void stretchModeChanged(bool isStretchMode);
+	void palletteChanged(TC001Pallette pallette);
+	void globalTemperatureChanged(double temperature);
+};
