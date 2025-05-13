81b2f1058df31150666a93d6c062b9d41a7a06f5
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-05-13 12:25:46 -0500

    add basic DeviceController

--- src/Controllers/DeviceController/devicecontroller.cpp
+++ src/Controllers/DeviceController/devicecontroller.cpp
@@ -6,3 +6,61 @@ DeviceController::DeviceController(QObject *parent)
 
 DeviceController::~DeviceController()
 {}
+
+void DeviceController::openDevices()
+{
+	for (Device* device : mDevices) {
+		if (device) {
+			device->open();
+		}
+	}
+	emit devicesOpened();
+}
+
+void DeviceController::startDevices()
+{
+	for (Device* device : mDevices) {
+		if (device) {
+			device->start();
+		}
+	}
+	emit devicesStarted();
+}
+
+void DeviceController::stopDevices()
+{
+	for (Device* device : mDevices) {
+		if (device) {
+			device->stop();
+		}
+	}
+	emit devicesStopped();
+}
+
+void DeviceController::closeDevices()
+{
+	for (Device* device : mDevices) {
+		if (device) {
+			device->close();
+		}
+	}
+	emit devicesClosed();
+}
+
+void DeviceController::addDevice(Device* device) {
+	if (device) {
+		mDevices.append(device);
+
+		emit deviceAdded(device);
+	}
+}
+
+void DeviceController::removeDevice(Device* device)
+{
+	if (device) {
+		mDevices.removeAll(device);
+		delete device; // CONSIDER: determine where to clean up
+	}
+
+	emit deviceRemoved(device);
+}
--- src/Controllers/DeviceController/devicecontroller.h
+++ src/Controllers/DeviceController/devicecontroller.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <Devices/Device/device.h>
 
 class DeviceController  : public QObject
 {
@@ -9,4 +10,27 @@ class DeviceController  : public QObject
 public:
 	DeviceController(QObject *parent);
 	~DeviceController();
+
+private:
+	QList<Device*> mDevices;
+
+public slots:
+	void addDevice(Device* device);
+	void removeDevice(Device* device);
+	Device* getDevice(int index) const;
+
+
+	void openDevices();
+	void startDevices();
+	void stopDevices();
+	void closeDevices();
+
+signals:
+	void devicesOpened();
+	void devicesStarted();
+	void devicesStopped();
+	void devicesClosed();
+
+	void deviceAdded(Device* device);
+	void deviceRemoved(Device* device);
 };
