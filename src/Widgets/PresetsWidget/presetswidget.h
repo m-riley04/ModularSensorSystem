4b9ff56f025ac431dc36da3a151c6bf597ba887b
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-05-14 13:35:19 -0500

    create skeleton for presets widget

---
+++ src/Widgets/PresetsWidget/presetswidget.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QWidget>
+#include "ui_presetswidget.h"
+
+class PresetsWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	PresetsWidget(QWidget *parent = nullptr);
+	~PresetsWidget();
+
+private:
+	Ui::PresetsWidgetClass ui;
+};
