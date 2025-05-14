541897c2cd5834964cb04847b2fca0c61c14a28d
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-05-14 13:35:09 -0500

    implement device list widget functionality

--- src/Widgets/DeviceListWidget/devicelistwidget.cpp
+++ src/Widgets/DeviceListWidget/devicelistwidget.cpp
@@ -1,10 +1,66 @@
 #include "devicelistwidget.h"
 
-DeviceListWidget::DeviceListWidget(QWidget *parent)
+DeviceListWidget::DeviceListWidget(QWidget* parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+	initSignals();
+}
+
+DeviceListWidget::DeviceListWidget(DeviceController* deviceController, QWidget* parent)
+	: QWidget(parent), pDeviceController(deviceController)
+{
+	ui.setupUi(this);
+	initSignals();
 }
 
 DeviceListWidget::~DeviceListWidget()
 {}
+
+void DeviceListWidget::initSignals()
+{
+	// UI button signals
+	connect(ui.buttonAdd, &QPushButton::clicked, this, &DeviceListWidget::openAddDeviceDialog);
+	//connect(ui.buttonRemove, &QPushButton::clicked, this, &DeviceListWidget::openRemoveDeviceDialog);
+
+	initDeviceControllerSignals();
+}
+
+void DeviceListWidget::initDeviceControllerSignals()
+{
+	if (pDeviceController == nullptr) return;
+
+	// Disconnect previous signals
+	disconnect(pDeviceController, &DeviceController::deviceAdded, this, nullptr);
+
+	// Connect device controller signals
+	connect(pDeviceController, &DeviceController::deviceAdded, this, [this](Device* device) {
+		if (device == nullptr) return;
+
+		// Add the device to the list
+		QString listItemName = device->id() + " (" + Device::typeToString(device->deviceType()) + ")";
+		ui.listDevices->addItem(listItemName);
+		});
+}
+
+void DeviceListWidget::setDeviceController(DeviceController* deviceController)
+{
+	if (deviceController == nullptr) return;
+	pDeviceController = deviceController;
+
+	// Connect signals
+	initDeviceControllerSignals();
+
+	emit deviceControllerChanged(deviceController);
+}
+
+void DeviceListWidget::openAddDeviceDialog()
+{
+	AddCameraDialog* addCameraDialog = new AddCameraDialog(this);
+
+	connect(addCameraDialog, &AddCameraDialog::deviceSelected, pDeviceController, &DeviceController::addCamera);
+
+	addCameraDialog->setAttribute(Qt::WA_DeleteOnClose);
+	addCameraDialog->setWindowModality(Qt::WindowModal);
+	addCameraDialog->show();
+}
--- src/Widgets/DeviceListWidget/devicelistwidget.h
+++ src/Widgets/DeviceListWidget/devicelistwidget.h
@@ -3,14 +3,29 @@
 #include <QWidget>
 #include "ui_devicelistwidget.h"
 
+#include "Windows/AddCameraDialog/AddCameraDialog.h"
+#include <Controllers/DeviceController/devicecontroller.h>
+
 class DeviceListWidget : public QWidget
 {
 	Q_OBJECT
 
 public:
-	DeviceListWidget(QWidget *parent = nullptr);
+	DeviceListWidget(QWidget* parent = nullptr);
+	DeviceListWidget(DeviceController* deviceController, QWidget *parent = nullptr);
 	~DeviceListWidget();
 
 private:
 	Ui::DeviceListWidgetClass ui;
+	DeviceController* pDeviceController;
+
+	void initSignals();
+	void initDeviceControllerSignals();
+
+public slots:
+	void openAddDeviceDialog();
+	void setDeviceController(DeviceController* deviceController);
+
+signals:
+	void deviceControllerChanged(DeviceController* deviceController);
 };
