6971a08d8db5e95b0ceab8644e31b4f38b97e62e
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-05-20 18:23:02 -0500

    remove unnecessary files from libraries

--- src/devicecore/DeviceCore/devicecore.cpp
+++
@@ -1,5 +0,0 @@
-#include "devicecore.h"
-
-DeviceCore::DeviceCore()
-{
-}
--- src/devicecore/DeviceCore/devicecore.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "devicecore_global.h"
-
-class DEVICECORE_EXPORT DeviceCore
-{
-public:
-    DeviceCore();
-};
