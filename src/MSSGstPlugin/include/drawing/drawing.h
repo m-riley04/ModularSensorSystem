afa2edd637ee63078c0f9321502c965330d618df
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-11-14 11:24:40 -0600

    use draw_rect for drawing

--- src/MSSGstPlugin/include/drawing/drawing.h
+++ src/MSSGstPlugin/include/drawing/drawing.h
@@ -21,6 +21,25 @@ clear_canvas(Canvas* canvas)
     }
 }
 
+/**
+ * @brief Draws a rectangle onto the canvas at the specified position, size, and color.
+ * @param canvas The canvas to draw on.
+ * @param pos The position to draw the rectangle at.
+ * @param size The size of the rectangle.
+ * @param color The color of the rectangle.
+ */
+static void
+draw_rect(Canvas* canvas, Position pos, Size size, Color color)
+{
+    for (guint y = pos.y; y < pos.y + size.height && y < canvas->size.height; y++) {
+        guint8* row = canvas->data + y * canvas->size.width * COLOR_SIZE_BYTES;
+        for (guint x = pos.x; x < pos.x + size.width && x < canvas->size.width; x++) {
+            guint8* px = row + x * COLOR_SIZE_BYTES;
+            set_color(px, color, canvas->format);
+        }
+    }
+}
+
 static void
 draw_bar_value(Canvas* canvas, gdouble value)
 {
@@ -35,26 +54,24 @@ draw_bar_value(Canvas* canvas, gdouble value)
 
     // Color based on value
 	Color color = COLOR_WHITE;
-    if (value < 0.60) {
+	const gdouble thresh_low = 0.60, thresh_med = 0.75, thresh_high = 0.90;
+    if (value < thresh_low) {
         color = COLOR_GREEN;
     }
-    else if (value < 0.75) {
+    else if (value < thresh_med) {
         color = COLOR_YELLOW;
     }
-    else if (value < 0.90) {
+    else if (value < thresh_high) {
         color = COLOR_ORANGE;
     }
     else {
         color = COLOR_RED;
 	}
 
-    for (guint y = y0; y < canvas->size.height; y++) {
-        guint8* row = canvas->data + y * canvas->size.width * COLOR_SIZE_BYTES;
-        for (guint x = x0; x < x0 + bar_w; x++) {
-            guint8* px = row + x * COLOR_SIZE_BYTES;
-            set_color(px, color, canvas->format);
-        }
-    }
+	// Use draw_rect to draw the bar
+    Position pos = { x0, y0 };
+    Size size = { bar_w, bar_h };
+	draw_rect(canvas, pos, size, color);
 }
 
 static void
